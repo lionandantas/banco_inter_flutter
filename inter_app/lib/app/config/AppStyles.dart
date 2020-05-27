0c475854f92cf9c770d02b5c66e270dd4ec29cc2
Lionan Dantas <=>
2020-05-27 18:30:25 -0300

    limpando codigo

--- inter_app/lib/app/config/AppStyles.dart
+++ inter_app/lib/app/config/AppStyles.dart
@@ -1,16 +1,9 @@
 import 'dart:ui';
-import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:interapp/app/utils/hexcolor.dart';
 
 class AppStyles {
-  // static Color primaryColor = HexColor("#10C66F");
-  static Color primaryColor =  HexColor("#FF6D08");//HexColor("#4F008D");HexColor("#1E0136");
+  static Color primaryColor = HexColor("#FF6D08");
   static Color greyMedium = HexColor("#f5f6fa");
   static List<Color> gradient = [Color(0xFFFF6D08), Color(0xFFF87C08)];
-  
-  static Brightness fidelBrightness =
-      Platform.isAndroid ? Brightness.dark : Brightness.light;
 }
-
-
